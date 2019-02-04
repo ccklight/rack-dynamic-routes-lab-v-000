0ae3da22a14c678e253c51bb3c3813212085af0c
Cheryl K <cherylkelson@icloud.com>
2019-02-04 15:15:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,5 @@
 class Application
 
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#
-#     if req.path=="/songs"
-#       resp.write "You requested the songs"
-#     else
-#       resp.write "Route not found"
-#       resp.status = 404
-#     end
-#
-#     resp.finish
-#   end
-# end
-# ********
 @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
 
   def call(env)
